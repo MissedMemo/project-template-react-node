525e2c1f902e1056d8f047e5fac5446274f233f9
AndyL <missedmemo@gmail.com>
2019-02-17 22:59:50 -0800

    add simple react source dir and place-holder code

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = ({greeting}) => <div>{ greeting }</div>
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './app'
+
+render(
+  <App greeting='Hello, Template!' />,
+  document.getElementById('root')
+)
